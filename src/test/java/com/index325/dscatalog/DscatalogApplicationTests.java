9cfede058627225323655900cae7b3967da7eb81
Gabriel Orlando <gabriel.orlando@monitoratec.com.br>
2022-07-19 21:23:25 -0300

    Initial commit

---
+++ src/test/java/com/index325/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.index325.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
