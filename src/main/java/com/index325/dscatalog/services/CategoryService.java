83d4176270915d27cd87ed1db67ce008c01ecc54
Gabriel Orlando <gabriel.orlando@monitoratec.com.br>
2022-07-20 22:35:49 -0300

    Adding custom exceptions

--- src/main/java/com/index325/dscatalog/services/CategoryService.java
+++ src/main/java/com/index325/dscatalog/services/CategoryService.java
@@ -3,6 +3,7 @@ package com.index325.dscatalog.services;
 import com.index325.dscatalog.dto.CategoryDTO;
 import com.index325.dscatalog.entities.Category;
 import com.index325.dscatalog.repositories.CategoryRepository;
+import com.index325.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -25,4 +27,16 @@ public class CategoryService {
         return categories.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> optionalCategory = categoryRepository.findById(id);
+
+//        if (!optionalCategory.isPresent()){
+//
+//        }
+
+        Category entity = optionalCategory.orElseThrow(() -> new EntityNotFoundException("Such category was not found"));
+
+        return new CategoryDTO(entity);
+    }
 }
